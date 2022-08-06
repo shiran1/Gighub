1181297f58c89c72ea91670e34c3ce8340f20250
shiran <mrz5522@gmail.com>
2022-08-06 11:38:44 +0530

    Add database Connection and Inti migrations

---
+++ GigHub/Migrations/Configuration.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace GigHub.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<GigHub.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+    } 
+}
