11e0a22ea1d3d54e56cf8eada82414fb59b95d30
shiran <mrz5522@gmail.com>
2022-08-06 16:57:59 +0530

    Implement validations for Gigs
    Signed-off-by: shiran <mrz5522@gmail.com>

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,25 +1,31 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using GigHub.Models;
 
 namespace GigHub.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
-        
-        public string Date { get; set; }
-        
-        public string Time { get; set; }
-        
+
+        [Required][FutureDate] public string Date { get; set; }
+
+        [Required][ValidTime] public string Time { get; set; }
+
         // For communicate with Main Model (Gig)
-        public byte Genre { get; set; }
+        [Required] public byte Genre { get; set; }
+
         // For Display list of Genres which are coming from GigsController
         public IEnumerable<Genre> Genres { get; set; }
 
-        
+
         // Information Expert = the one who has the detail is the one who should do it
-        public DateTime DateTime => DateTime.Parse($"{Date} {Time}");
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format($"{Date} {Time}"));
+        }
     }
 }
